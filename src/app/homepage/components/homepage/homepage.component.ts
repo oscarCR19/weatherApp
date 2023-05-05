de51c5b381112eea54db7c8d2db457207c1394e1
oscarCR19 <oscarmora2006@gmail.com>
2023-05-04 22:05:04 -0600

    Implementando geolocalizacion

--- src/app/homepage/components/homepage/homepage.component.ts
+++ src/app/homepage/components/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,ElementRef} from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 
 @Component({
@@ -6,6 +7,42 @@ import { Component } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+
+
+
+export class HomepageComponent implements OnInit {
+
+  geocoder!: google.maps.Geocoder;
+
+  constructor(private elementRef: ElementRef) {
+    this.geocoder = new google.maps.Geocoder();
+  }
+
+
+   ngOnInit(): void {
+    async () => {
+      let coordinates = await Geolocation. getCurrentPosition();
+
+      console.log('Current position:', coordinates);
+    };
+
+    //this.getAddress(coordinates);
+
+   }
+
+   getAddress(latitude: number, longitude: number) {
+    const location = new google.maps.LatLng(latitude, longitude);
+
+    this.geocoder.geocode({ location }, (results:any, status:any) => {
+      if (status === 'OK') {
+        if (results[1]) {
+          console.log(results[1].formatted_address);
+        }
+      } else {
+        console.error('Geocoder failed due to: ' + status);
+      }
+    });
+  }
+
 
 }
